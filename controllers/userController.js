e6fef3a122d4c298187147a8bb89e9774a5520f7
brachtco <brachtdanny@gmail.com>
2023-03-30 11:19:22 -0600

    Revise userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,14 +1,15 @@
-const { User, Thought } = require("../models");
+const { User, Thought } = require("../models/");
 
 module.exports = {
   getUser(req, res) {
     User.find()
       .then((user) => res.json(user))
       .catch((err) => res.status(500).json(err));
+      console.log(err);
   },
 
   getSingleUser(req, res) {
-    User.findOne({ _id: req.oarams.userId })
+    User.findOne({ _id: req.params.userId })
       .populate("thoughts")
       .populate("friends")
       .select("-__v")
